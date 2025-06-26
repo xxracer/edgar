c069aa261edbefa3c388acca2ecfd630ca17a38c
Maijel Cancines <maijelcancines2@gmail.com>
2025-06-26 01:46:29 +0000

    quiero colocar  este link de la imagen https://www.canva.com/design/DAGr

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.canva.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
